2ad4232f75620705baa70bfe567fc065d9d46532
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-04-11 20:27:58 +0700

    Add files via upload

---
+++ data/PointofInterestKudus_2.js
@@ -0,0 +1,17 @@
+var json_PointofInterestKudus_2 = {
+"type": "FeatureCollection",
+"name": "PointofInterestKudus_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Makam Sunan Kudus", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.832212471371491, -6.80392474548926 ] } },
+{ "type": "Feature", "properties": { "Name": "Masjid Al-Aqsha Menara Kudus", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.832837169797941, -6.804168461915722 ] } },
+{ "type": "Feature", "properties": { "Name": "Makam Mbah Kyai Telingsing", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.833954203419239, -6.810395136478024 ] } },
+{ "type": "Feature", "properties": { "Name": "Kudus Great Mosque", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.841243985841729, -6.807268142672974 ] } },
+{ "type": "Feature", "properties": { "Name": "Museum Jenang Kudus", "X": null, "Y": null, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 110.843763439417344, -6.802534373477938 ] } },
+{ "type": "Feature", "properties": { "Name": "RS Kartika Husada", "X": 110.8459109, "Y": -6.806578099, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.845870980930158, -6.806877141856608 ] } },
+{ "type": "Feature", "properties": { "Name": "Rumah Sakit dr. Loekmono Hadi Kudus", "X": 110.8365349, "Y": -6.81862776, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.8365349, -6.81862776 ] } },
+{ "type": "Feature", "properties": { "Name": "Titipan Sepeda & Motor Barokah", "X": 110.8364712, "Y": -6.816105747, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.8364712, -6.816105747 ] } },
+{ "type": "Feature", "properties": { "Name": "Polsek Kota Kudus", "X": 110.8478478, "Y": -6.80599189, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.8478478, -6.80599189 ] } },
+{ "type": "Feature", "properties": { "Name": "SPBU Matahari", "X": 110.8373791, "Y": -6.818243701, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 110.837379800334546, -6.818575659580872 ] } }
+]
+}
